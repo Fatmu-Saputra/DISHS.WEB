a9846da15ef11636cb4a19b38214e4ab686ae477
Fatmu-Saputra <adissaputra.karier@gmail.com>
2023-10-05 22:36:44 +0700

    DISHS

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik di luar side bar untuk menghilangkan hamburger menu
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
